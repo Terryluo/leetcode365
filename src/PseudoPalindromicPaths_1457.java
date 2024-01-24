100304a6de5a9b9218e882a7c424439b21b80035
Zhiteng Luo(Terry) <terryluo256@gmail.com>
2024-01-24 13:51:28 -0500

    PseudoPalindromicPaths

---
+++ src/PseudoPalindromicPaths_1457.java
@@ -0,0 +1,77 @@
+import util.TreeNode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+1457. Pseudo-Palindromic Paths in a Binary Tree
+
+Given a binary tree where node values are digits from 1 to 9. A path in the binary tree is said to be pseudo-palindromic if at least one permutation of the node values in the path is a palindrome.
+
+Return the number of pseudo-palindromic paths going from the root node to leaf nodes.
+
+Example 1:
+
+
+
+Input: root = [2,3,1,3,1,null,1]
+Output: 2
+Explanation: The figure above represents the given binary tree. There are three paths going from the root node to leaf nodes: the red path [2,3,3], the green path [2,1,1], and the path [2,3,1]. Among these paths only red path and green path are pseudo-palindromic paths since the red path [2,3,3] can be rearranged in [3,2,3] (palindrome) and the green path [2,1,1] can be rearranged in [1,2,1] (palindrome).
+Example 2:
+
+
+
+Input: root = [2,1,1,1,3,null,null,null,null,null,1]
+Output: 1
+Explanation: The figure above represents the given binary tree. There are three paths going from the root node to leaf nodes: the green path [2,1,1], the path [2,1,3,1], and the path [2,1]. Among these paths only the green path is pseudo-palindromic since [2,1,1] can be rearranged in [1,2,1] (palindrome).
+Example 3:
+
+Input: root = [9]
+Output: 1
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 10^5].
+1 <= Node.val <= 9
+* */
+public class PseudoPalindromicPaths_1457 {
+    public int pseudoPalindromicPaths (TreeNode root) {
+        //Note that the node values of a path form a palindrome if at most one digit has an odd frequency (parity).
+        // Use a Depth First Search (DFS) keeping the frequency (parity) of the digits. Once you are in a leaf node check if at most one digit has an odd frequency (parity).
+        int[] result = new int[1];
+        Map<Integer, Integer> record = new HashMap<>();
+        dfs(root, record, result);
+        return result[0];
+    }
+    private void dfs(TreeNode root, Map<Integer, Integer> record, int[] result) {
+        // base case
+        if (root == null) {
+            return;
+        }
+        record.put(root.val, record.getOrDefault(root.val, 0) + 1);
+        if (root.left == null && root.right == null) {
+            // at the leaf
+            int freq = 0;
+            for (Map.Entry<Integer, Integer> entry : record.entrySet()) {
+                if (entry.getValue() % 2 != 0) {
+                    freq++;
+                }
+            }
+            if (freq <= 1) {
+                result[0]++;
+            }
+        }
+        dfs(root.left, record, result);
+        dfs(root.right, record, result);
+        record.put(root.val, record.getOrDefault(root.val, 0) - 1);
+    }
+
+    public static void main(String[] args) {
+        PseudoPalindromicPaths_1457 ppp = new PseudoPalindromicPaths_1457();
+        TreeNode root = TreeNode.reconstructTreeForLeetcode(Arrays.asList(2,3,1,3,1,null,1));
+        int result = ppp.pseudoPalindromicPaths(root);
+        System.out.println(result);
+    }
+}
