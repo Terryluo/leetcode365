de3da57c3c2cf77450736963bdc59d4f612d7a58
Zhiteng Luo(Terry) <terryluo256@gmail.com>
2024-07-29 21:12:04 -0500

    count number of teams

---
+++ src/CountNumberofTeams_1395.java
@@ -0,0 +1,107 @@
+/*
+1395. Count Number of Teams
+Medium
+
+There are n soldiers standing in a line. Each soldier is assigned a unique rating value.
+
+You have to form a team of 3 soldiers amongst them under the following rules:
+
+Choose 3 soldiers with index (i, j, k) with rating (rating[i], rating[j], rating[k]).
+A team is valid if: (rating[i] < rating[j] < rating[k]) or (rating[i] > rating[j] > rating[k]) where (0 <= i < j < k < n).
+Return the number of teams you can form given the conditions. (soldiers can be part of multiple teams).
+
+
+
+Example 1:
+
+Input: rating = [2,5,3,4,1]
+Output: 3
+Explanation: We can form three teams given the conditions. (2,3,4), (5,4,1), (5,3,1).
+Example 2:
+
+Input: rating = [2,1,3]
+Output: 0
+Explanation: We can't form any team given the conditions.
+Example 3:
+
+Input: rating = [1,2,3,4]
+Output: 4
+
+
+Constraints:
+
+n == rating.length
+3 <= n <= 1000
+1 <= rating[i] <= 105
+All the integers in rating are unique.
+ */
+public class CountNumberofTeams_1395 {
+    public int numTeams(int[] rating) {
+        // brute force solution
+        int result = 0;
+        for (int i = 0; i < rating.length - 2; i++) {
+            for (int j = i + 1; j < rating.length - 1; j++) {
+                for (int k = j + 1; k < rating.length; k++) {
+                    if ((rating[i] < rating[j] && rating[j] < rating[k]) || (rating[i] > rating[j] && rating[j] > rating[k])) {
+                        result++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    // best solution
+    public int numTeams1(int[] rating) {
+        int n = rating.length;
+        if (n < 3) return 0;
+
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int num : rating) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        int[] leftTree = new int[max - min + 2];
+        int[] rightTree = new int[max - min + 2];
+
+        for (int num : rating) {
+            update(rightTree, num - min, 1);
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            update(rightTree, rating[i] - min, -1);
+
+            int lessLeft = query(leftTree, rating[i] - min - 1);
+            int greaterLeft = i - lessLeft;
+
+            int lessRight = query(rightTree, rating[i] - min - 1);
+            int greaterRight = (n - i - 1) - lessRight;
+
+            count += lessLeft * greaterRight + greaterLeft * lessRight;
+
+            update(leftTree, rating[i] - min, 1);
+        }
+
+        return count;
+    }
+
+    private void update(int[] tree, int index, int value) {
+        index++;
+        while (index < tree.length) {
+            tree[index] += value;
+            index += index & (-index);
+        }
+    }
+
+    private int query(int[] tree, int index) {
+        int sum = 0;
+        index++;
+        while (index > 0) {
+            sum += tree[index];
+            index -= index & (-index);
+        }
+        return sum;
+    }
+}
